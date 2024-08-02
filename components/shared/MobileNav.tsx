3c67e90e1c6a927111959cb58ce066e74492de90
akhil683 <onkar.palsra@gmail.com>
2024-08-03 00:53:42 +0530

    connect db & hero section

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Image from "next/image";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="logo"
+          />
+          <Separator className="border border-gray-100" />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
 };
 
 export default MobileNav;
